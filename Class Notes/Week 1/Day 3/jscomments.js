cb592c52f0022465aff63ba85e29b1d15ec4f8af
btaylorpersevere <btaylor@perseverenow.org>
2024-04-10 08:00:04 -0500

    updated notes with comments

---
+++ Class Notes/Week 1/Day 3/jscomments.js
@@ -0,0 +1,16 @@
+// This is the first comment  
+// This is the second comment  
+// I am a single line comment
+
+/*
+This is a multiline comment  
+ Multiline comments can take multiple lines  
+ JavaScript is the language of the web  
+ */
+
+ console.log(2 + 3) // Addition
+console.log(3 - 2) // Subtraction
+console.log(2 * 3) // Multiplication
+console.log(3 / 2) // Division
+console.log(3 % 2) // Modulus - finding remainder
+console.log(3 ** 2) // Exponentiation 3 ** 2 == 3 * 3

fcb4e6249a27e9ca4629bb60108ddda1ed57a1e3
btaylorpersevere <btaylor@perseverenow.org>
2024-04-18 09:53:33 -0500

    updated homework

--- Class Notes/Week 2/Day 2/Numbers/strings.js
+++ Class Notes/Week 2/Day 2/Numbers/strings.js
@@ -1,6 +1,6 @@
-// //Strings are texts, which are under single , double, back-tick quote. 
-// //To declare a string, we need a variable name, assignment operator, a value under a single quote, double quote, or backtick quote. Let's see some 
-// //examples of strings:
+//Strings are texts, which are under single , double, back-tick quote. 
+//To declare a string, we need a variable name, assignment operator, a value under a single quote, double quote, or backtick quote. Let's see some 
+//examples of strings:
 
 // let space = ' '           // an empty space string
 // let firstName = 'Asabeneh'
@@ -12,45 +12,45 @@
 // let quote = "The saying,'Seeing is Believing' is not correct in 2020."
 // let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
 
-// //String concatenation
+//String concatenation
 
-// //Connecting two or more strings together is called concatenation. Using the ///strings declared in the previous String section:
+//Connecting two or more strings together is called concatenation. Using the ///strings declared in the previous String section:
 
 // let fullName = firstName + space + lastName; // concatenation, merging two string together.
 // console.log(fullName);
 
-// // We can concatenate strings in different ways.
+// We can concatenate strings in different ways.
 
-// // Concatenating Using Addition Operator
-// // Concatenating using the addition operator is an old way. This way of concatenating is tedious and error-prone. It is good to know how to concatenate this way, but I strongly suggest to use the ES6 template strings (explained later on).
+// Concatenating Using Addition Operator
+// Concatenating using the addition operator is an old way. This way of concatenating is tedious and error-prone. It is good to know how to concatenate this way, but I strongly suggest to use the ES6 template strings (explained later on).
 
-// // Declaring different variables of different data types
-// let space = ' '
-// let firstName = 'Asabeneh'
-// let lastName = 'Yetayeh'
-// let country = 'Finland'
-// let city = 'Helsinki'
-// let language = 'JavaScript'
-// let job = 'teacher'
-// let age = 250
+// Declaring different variables of different data types
+let space = ' '
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let age = 250
 
 
-// let fullName =firstName + space + lastName
-// let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+let fullName =firstName + space + lastName
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
 
-// console.log(personInfoOne)
+console.log(country)
 
-// //Long Literal Strings
-// // A string could be a single character or paragraph or a page. If the string length is too big it does not fit in one line. We can use the backslash character (\) at the end of each line to indicate that the string will continue on the next line. Example:
+//Long Literal Strings
+// A string could be a single character or paragraph or a page. If the string length is too big it does not fit in one line. We can use the backslash character (\) at the end of each line to indicate that the string will continue on the next line. Example:
 
-// const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
-// I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
-// Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
-// In the end of 2019, I was thinking to expand my teaching and to reach \
-// to global audience and I started a Python challenge from November 20 - December 19.\
-// It was one of the most rewarding and inspiring experience.\
-// Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
-// I hope you are enjoying too."
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
 
-// console.log(paragraph)
+console.log(paragraph)
 
---
+++ Class Notes/Week 2/Day 4/templiterals.js
@@ -0,0 +1,141 @@
+// /*Template Literals (Template Strings)
+// To create a template strings, we use two back-ticks. We can inject data as expressions inside a template string. To inject data, we enclose the expression with a curly bracket({}) preceded by a $ sign. See the syntax below.*/
+
+// //Syntax
+// `String literal text`
+// `String literal text ${expression}`
+
+// console.log(`The sum of 2 and 3 is 5`)              // statically writing the data
+// let a = 2
+// let b = 3
+// console.log(`The sum of ${a} and ${b} is ${a + b}`) // injecting the data dynamically
+
+// let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// let country = 'Finland'
+// let city = 'Helsinki'
+// let language = 'JavaScript'
+// let job = 'teacher'
+// let age = 250
+// let fullName = firstName + ' ' + lastName
+
+// let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.` //ES6 - String interpolation method
+// let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I teach ${language}.`
+// console.log(personInfoTwo)
+// console.log(personInfoThree)
+
+// //Using a string template or string interpolation method, we can add expressions, which could be a value, or some operations (comparison, arithmetic operations, ternary operation).
+
+// let a = 2
+// let b = 3
+// console.log(`${a} is greater than ${b}: ${a > b}`)
+
+
+// //String Methods
+// Everything in JavaScript is an object. A string is a primitive data type that means we can not modify it once it is created. The string object has many string methods. There are different string methods that can help us to work with strings.
+
+// //length: The string length method returns the number of characters in a string included empty space.
+
+// let js = 'JavaScript'
+// console.log(js.length)         // 10
+// let firstName = 'Asabeneh'
+// console.log(firstName.length)  // 8
+
+// //Accessing characters in a string: We can access each character in a string using its index. In programming, counting starts from 0. The first index of the string is zero, and the last index is the length of the string minus one.
+
+// let string = 'JavaScript'
+// let firstLetter = string[0]
+
+// console.log(firstLetter)           // J
+
+// let secondLetter = string[1]       // a
+// let thirdLetter = string[2]
+// let lastLetter = string[9]
+
+// console.log(lastLetter)            // t
+
+// let lastIndex = string.length - 1
+
+// console.log(lastIndex)  // 9
+// console.log(string[lastIndex])    // t
+
+// //toUpperCase(): this method changes the string to uppercase letters.
+
+// let string = 'JavaScript'
+
+// console.log(string.toUpperCase())     // JAVASCRIPT
+
+// let firstName = 'Asabeneh'
+
+// console.log(firstName.toUpperCase())  // ASABENEH
+
+// let country = 'Finland'
+
+// console.log(country.toUpperCase())    // FINLAND
+
+// //toLowerCase(): this method changes the string to lowercase letters.
+// let string = 'JavasCript'
+
+// console.log(string.toLowerCase())     // javascript
+
+// let firstName = 'Asabeneh'
+
+// console.log(firstName.toLowerCase())  // asabeneh
+
+// let country = 'Finland'
+
+// console.log(country.toLowerCase())   // finland
+
+
+
+// //substr(): It takes two arguments, the starting index and number of characters to slice.
+
+// let string = 'JavaScript'
+// console.log(string.substr(4,6))    // Script
+
+// let country = 'Finland'
+// console.log(country.substr(3, 4))   // land
+
+// //substring(): It takes two arguments, the starting index and the stopping index but it doesn't include the character at the stopping index.
+
+// let string = 'JavaScript'
+
+// console.log(string.substring(0,4))     // Java
+// console.log(string.substring(4,10))    // Script
+// console.log(string.substring(4))       // Script
+
+// let country = 'Finland'
+
+// console.log(country.substring(0, 3))   // Fin
+// console.log(country.substring(3, 7))   // land
+// console.log(country.substring(3))      // land
+
+// //split(): The split method splits a string at a specified place.
+
+// let string = '30 Days Of JavaScript'
+
+// console.log(string.split())     // Changes to an array -> ["30 Days Of JavaScript"]
+// console.log(string.split(' '))  // Split to an array at space -> ["30", "Days", "Of", "JavaScript"]
+
+// let firstName = 'Asabeneh'
+
+// console.log(firstName.split())    // Change to an array - > ["Asabeneh"]
+// console.log(firstName.split(''))  // Split to an array at each letter ->  ["A", "s", "a", "b", "e", "n", "e", "h"]
+
+// let countries = 'Finland, Sweden, Norway, Denmark, and Iceland'
+
+// console.log(countries.split(','))  // split to any array at comma -> ["Finland", " Sweden", " Norway", " Denmark", " and Iceland"]
+// console.log(countries.split(', ')) //  ["Finland", "Sweden", "Norway", "Denmark", "and Iceland"]
+
+// trim(): Removes trailing space in the beginning or the end of a string.
+
+// let string = '   30 Days Of JavaScript   '
+
+// console.log(string)
+// console.log(string.trim(' '))
+
+// let firstName = ' Asabeneh '
+
+// console.log(firstName)
+// console.log(firstName.trim())  // still removes spaces at the beginning and the end of the string
+

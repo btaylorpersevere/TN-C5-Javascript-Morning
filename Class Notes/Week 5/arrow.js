fbeae933ba53fac4bfc9bb07a71d8eeab159b176
btaylorpersevere <btaylor@perseverenow.org>
2024-05-07 09:20:04 -0500

    updated homework

---
+++ Class Notes/Week 5/arrow.js
@@ -0,0 +1,93 @@
+
+// Sometimes we do not know how many arguments the user going to pass. Therefore, we should know how to write a function which can take unlimited number of arguments. The way we do it has a significant difference between a function declaration(regular function) and arrow function. Let us see examples both in function declaration and arrow function.
+
+
+// Unlimited number of parameters in regular function
+// A function declaration provides a function scoped arguments array like object. Any thing we passed as argument in the function can be accessed from arguments object inside the functions. Let us see an example
+
+
+// Let us access the arguments object
+​
+function sumAllNums() {
+ console.log(arguments)
+}
+
+sumAllNums(1, 2, 3, 4)
+// Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+
+// function declaration
+​
+function sumAllNums() {
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
+
+
+//Arrow Functions
+
+Unlimited number of parameters in arrow function
+Arrow function does not have the function scoped arguments object. To implement a function which takes unlimited number of arguments in an arrow function we use spread operator followed by any parameter name. Any thing we passed as argument in the function can be accessed as array in the arrow function. Let us see an example
+
+// Let us access the arguments object
+​
+const sumAllNums = (...args) => {
+ // console.log(arguments), arguments object not found in arrow function
+ // instead we use a parameter followed by spread operator (...)
+ console.log(args)
+}
+
+sumAllNums(1, 2, 3, 4)
+// [1, 2, 3, 4]
+
+// function declaration
+​
+const sumAllNums = (...args) => {
+  let sum = 0
+  for (const element of args) {
+    sum += element
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
+
+//anonymous function
+//Anonymous function or without name
+
+const anonymousFun = function() {
+    console.log(
+      'I am an anonymous function and my value is stored in anonymousFun'
+    )
+  }
+
+  //Expression Function
+// Expression functions are anonymous functions. After we create a function without a name and we assign it to a variable. To return a value from the function we should call the variable. Look at the example below.
+
+
+// Function expression
+const square = function(n) {
+    return n * n
+  }
+  
+  console.log(square(2)) // -> 4
+
+  //Self Invoking functions - an anonymous function which do not need to be called to return a value
+  (function(n) {
+    console.log(n * n)
+  })(2) // 4, but instead of just printing if we want to return and store the data, we do as shown below
+  
+  let squaredNum = (function(n) {
+    return n * n
+  })(10)
+  
+  console.log(squaredNum)

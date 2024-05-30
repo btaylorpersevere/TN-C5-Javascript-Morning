761e9f65d5f93a0f1b60823d31df63399ddb175a
btaylorpersevere <btaylor@perseverenow.org>
2024-05-30 09:56:13 -0500

    updated homework

---
+++ Class Assignment/Week 3/practice.js
@@ -0,0 +1,40 @@
+// function myFunction() {
+//     let person = prompt("Please enter your name", "Harry Potter");
+//     if (person != null) {
+//         console.log
+//       ("Hello " + person + "! How are you today?");}
+//     }
+//     myFunction()
+
+
+//Print a message for stating that if you are 21 years or older, you can purchase an alcoholic
+// //beverage
+// let age = 12
+// if (age >= 21) {
+//     console.log("You are eligible to purchase  drink")
+// } else if (age >= 16) {
+//     console.log("You are not eligible to purchase  drink")
+// } else {
+//     console.log('drink milk')
+// }
+let score 
+if (score >= 80 && score <= 100) {
+    console.log("you get an A")
+} else if (score >= 70 && score <= 79) {
+    console.log('you get a "B" ')
+} else if (score >=60 && score <=69) {
+    console.log('you get a "C" ')
+} else if (score >= 50 && score <= 59) {
+    console.log('you get a "D" ')
+} else if (score >= 0 && score <= 49) {
+    console.log('you get an "F" ')
+} else {
+    console.log('you need to take the test ')
+}
+
+    // 80-100, A
+    // 70-79, B
+    // 60-69, C
+    // 50-59, D
+    // 0-49, F
+    

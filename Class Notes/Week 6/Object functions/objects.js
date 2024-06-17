e774e62f34f6794b20dd9564e0a88858a4b9e52a
btaylorpersevere <btaylor@perseverenow.org>
2024-06-17 09:36:24 -0500

    updated homework

--- Class Notes/Week 6/Object functions/objects.js
+++ Class Notes/Week 6/Object functions/objects.js
@@ -1,72 +1,196 @@
-//Everything can be an object and objects do have properties and properties have values, so an object is a key value pair. The order of the key is not reserved, or there is no order. To create an object literal, we use two curly brackets.
+// Scope
+// Variable is the fundamental part in programming. We declare variable to store different data types. To declare a variable we use the key word var, let and const. A variable can be declared at different scope. In this section, we will see the scope variables, scope of variables when we use var or let. Variables scopes can be:
 
-//creating an empty object
+// Global
+// Local
+// Variable can be declared globally or locally scope. We will see both global and local scope. Anything declared without let, var or const is scoped at global level.
 
-// const person = {}
+// Let us imagine that we have a scope.js file.
 
-//Creating an objecting with values
-// Now, the person object has firstName, lastName, age, location, skills and isMarried properties. The value of properties or keys could be a string, number, boolean, an object, null, undefined or a function.
+// Window Global Object
+// Without using console.log() open your browser and check, you will see the value of a and b if you write a or b on the browser. That means a and b are already available in the window.
+let above
+let A
+let B
+//scope.js
+a = 'JavaScript' // declaring a variable without let or const make it available in window object and this found anywhere
+b = 10 // this is a global scope variable and found in the window object
 
-  const person = {
-    firstName: 'Asabeneh',
-    lastName: 'Yetayeh',
-    age: 250,
-    country: 'Finland',
-    city: 'Helsinki',
-    skills: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-      'Node',
-      'MongoDB',
-      'Python',
-      'D3.js'
-    ],
-    isMarried: true
+function letsLearnScope() {
+
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
   }
-  console.log(person)
+}
+console.log(a, b) // accessible
+// Global scope
+// A globally declared variable can be accessed every where in the same file. But the term global is relative. It can be global to the file or it can be global relative to some block of codes.
 
-  //Getting values from an object
-// We can access values of object using two methods:
+//scope.js
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+let c = 5
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  if (true) {
+    let a = 'Python'
+    let b = 100
+    console.log(a, b) // Python 100
+  console.log(a, b)
+}
+console.log(c)
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
 
-// using . followed by key name if the key-name is a one word
-// using square bracket and a quote
+// Local scope
+// A variable declared as local can be accessed only in certain block code.
+
+// Block Scope
+// Function Scope
+//scope.js
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+// Function scope
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  let value = false
+// block scope
+  if (true) {
+    // we can access from the function and outside the function but 
+    // variables declared inside the if will not be accessed outside the if block
+    let a = 'Python'
+    let b = 20
+    let c = 30
+    let d = 40
+    value = !value
+    console.log(a, b, c, value) // Python 20 30 true
+  }
+  // we can not access c because c's scope is only the if block
+  console.log(a, b, value) // JavaScript 10 true
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
+Now, you have an understanding of scope. A variable declared with var only scoped to function but variable declared with let or const is block scope(function block, if block, loop block, etc). Block in JavaScript is a code in between two curly brackets ({}).
+
+//scope.js
+function letsLearnScope() {
+  var gravity = 9.81
+  console.log(gravity)
+
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+if (true){
+  var gravity = 9.81
+  console.log(gravity) // 9.81
+}
+console.log(gravity)  // 9.81
+
+for(var i = 0; i < 3; i++){
+  console.log(i) // 0, 1, 2
+}
+console.log(i) // 3
+In ES6 and above there is let and const, so you will not suffer from the sneakiness of var. When we use let our variable is block scoped and it will not infect other parts of our code.
+
+//scope.js
+function letsLearnScope() {
+  // you can use let or const, but gravity is constant I prefer to use const
+  const gravity = 9.81
+  console.log(gravity)
+
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+if (true){
+  const  gravity = 9.81
+  console.log(gravity) // 9.81
+}
+// console.log(gravity), Uncaught ReferenceError: gravity is not defined
+
+for(let i = 0; i < 3; i++){
+  console.log(i) // 0, 1, 2
+}
+// console.log(i), Uncaught ReferenceError: i is not defined
+// The scope let and const are the same. The difference is only reassigning. We can not change or reassign the value of the const variable. I would strongly suggest you to use let and const, by using let and const you will write clean code and avoid hard to debug mistakes. As a rule of thumb, you can use let for any value which change, const for any constant value, and for an array, object, arrow function and function expression.
+
+📔 Object
+Everything can be an object and objects do have properties and properties have values, so an object is a key value pair. The order of the key is not reserved, or there is no order. To create an object literal, we use two curly brackets.
+
+Creating an empty object
+An empty object
+
+const person = {}
+Creating an objecting with values
+Now, the person object has firstName, lastName, age, location, skills and isMarried properties. The value of properties or keys could be a string, number, boolean, an object, null, undefined or a function.
+
+Let us see some examples of object. Each key has a value in the object.
+
+const rectangle = {
+  length: 20,
+  width: 20
+}
+console.log(rectangle) // {length: 20, width: 20}
 
 const person = {
-    firstName: 'Asabeneh',
-    lastName: 'Yetayeh',
-    age: 250,
-    country: 'Finland',
-    city: 'Helsinki',
-    skills: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-      'Node',
-      'MongoDB',
-      'Python',
-      'D3.js'
-    ],
-    getFullName: function() {
-      return `${this.firstName}${this.lastName}`
-    },
-    'phone number': '+3584545454545'
-  }
-  
-  // accessing values using .
-  console.log(person.firstName)
-  console.log(person.lastName)
-  console.log(person.age)
-  console.log(person.location) // undefined
-  
-  // value can be accessed using square bracket and key name
-  console.log(person['firstName'])
-  console.log(person['lastName'])
-  console.log(person['age'])
-  console.log(person['age'])
-  console.log(person['location']) // undefined
-  
-  // for instance to access the phone number we only use the square bracket method
-  console.log(person['phone number'])
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  isMarried: true
+}
+console.log(person)
+Getting values from an object
+We can access values of object using two methods:
+
+using . followed by key name if the key-name is a one word
+using square bracket and a quote
+const person = {
+  firstName: 'Asabeneh', //key valuie pair
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  getFullName: function() {
+    return `${this.firstName}${this.lastName}`
+  },
+  'phone number': '+3584545454545'
+}
+
+// accessing values using .
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+console.log(person.location) // undefined
+
+// value can be accessed using square bracket and key name
+console.log(person['firstName'])
+console.log(person['lastName'])
+console.log(person['age'])
+console.log(person['age'])
+console.log(person['location']) // undefined
+
+// for instance to access the phone number we only use the square bracket method
+console.log(person['phone number'])
